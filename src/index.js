ef69e57426c822be8c1345ae9ff28e8090912336
herbeysoft-team <herbeysoft@gmail.com>
2022-05-19 12:53:28 +0100

    Complete Authorization Login Register AWT with MySQL

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "bulma/css/bulma.css";
+import axios from "axios";
 import reportWebVitals from './reportWebVitals';
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
